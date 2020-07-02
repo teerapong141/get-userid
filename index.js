f4b7e72c12c46c54868b62e2d23a11106147da7d
teerapong141 <teerapong@npu.ac.th>
2020-07-02 10:36:00 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const functions = require('firebase-functions');
+const request = require('request-promise');
+
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
+const LINE_HEADER = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer wWIEG6M546F8oZ/95CvRFnQmnDMs4tskR9XhcO+hGAKIbh+8HVcl4TnVGryHVjwH+DwkW01eyW5a2SO0Exqi+CYRqXEj4Y6FslaBeClGYWnhcfKgB5GOKfXMUg+/KEfd6xr4KI4atazkZMEXgLfuk1GUYhWQfeY8sLGRXgo3xvw='
+};
+exports.LineBotReply = functions.https.onRequest((req, res) => {
+  if (req.method === "POST"){
+    reply(req.body);
+  } else {
+    return res.status(200).send(`Done`);
+  }
+});
+
+const reply = bodyResponse => {
+  return request({
+    method: `POST`,
+    uri: LINE_MESSAGING_API,
+    headers: LINE_HEADER,
+    body: JSON.stringify({
+      replyToken: bodyResponse.events[0].replyToken,
+      messages: [{
+        type: `text`,
+        text: JSON.stringify(bodyResponse)
+      }]
+    })
+  });
+};
